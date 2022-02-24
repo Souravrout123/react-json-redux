335997aad2ab45547559a792ee8f7ac83a0af7eb
Souravrout123 <iamsouravrout@gmail.com>
2022-02-24 07:38:11 +0530

    react-json-redux

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import {Routes,Route} from "react-router-dom";
+import Home from './pages/Home';
+import React from 'react';
+import AddUser from "./pages/AddUser";
+import EditUser from "./pages/EditUser";
+
+function App() {
+  return (
+    <div className="App">
+     <Routes>
+     <React.Fragment>
+     <Route path="/" element={<Home />}></Route>
+     <Route path="/addUser" element={<AddUser />}></Route>
+     <Route path="/editUser/:id" element={<EditUser />}></Route>     
+       </React.Fragment>
+     </Routes>
+    </div>
+  );
+}
+
+export default App;
